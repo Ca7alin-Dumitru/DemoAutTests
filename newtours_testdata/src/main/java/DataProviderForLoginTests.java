94f1406ee72e04f6653705791f1572bf73840664
Ca7alin-Dumitru <georgecatalin.dumitru@gmail.com>
2019-08-19 12:35:15 +0300

    testdata from excel

--- newtours_testdata/src/main/java/DataProviderForLoginTests.java
+++ newtours_testdata/src/main/java/DataProviderForLoginTests.java
@@ -19,7 +19,7 @@ public class DataProviderForLoginTests {
         ClassLoader classLoader = ClassLoader.getSystemClassLoader();
         File input = new File(classLoader.getResource("Login.xlsx").getFile());
 
-        if(input.exists() && !input.isDirectory()) {
+        if(true) {
             String myArray[][] = new String[2][2];
             int i = 0;
             try {
@@ -60,53 +60,6 @@ public class DataProviderForLoginTests {
             }
 
             return myArray;
-        }else{
-            File inputJenkins = new File("newtours_testdata/src/main/resources/Login.xlsx");
-            if(inputJenkins.exists() && !inputJenkins.isDirectory()) {
-                String myArray[][] = new String[2][2];
-                int i = 0;
-                try {
-                    FileInputStream file = new FileInputStream(input);
-
-                    // Create Workbook instance holding reference to .xlsx file
-                    HSSFWorkbook workbook = new HSSFWorkbook(file);
-
-                    // Get first/desired sheet from the workbook
-                    HSSFSheet sheet = workbook.getSheetAt(0);
-
-                    // Iterate through each rows one by one
-                    Iterator<Row> rowIterator = sheet.iterator();
-                    while (rowIterator.hasNext()) {
-                        Row row = rowIterator.next();
-                        // For each row, iterate through all the columns
-                        Iterator<Cell> cellIterator = row.cellIterator();
-                        int j = 0;
-                        while (cellIterator.hasNext()) {
-                            Cell cell = cellIterator.next();
-                            // Check the cell type and format accordingly
-                            switch (cell.getCellType()) {
-                                case NUMERIC:
-                                    myArray[i][j] = Double.toString(cell.getNumericCellValue());
-                                    break;
-                                case STRING:
-                                    myArray[i][j] = cell.getStringCellValue();
-                                    break;
-                            }
-                            j++;
-                        }
-                        i++;
-                    }
-                    file.close();
-                }
-                catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-                return myArray;
-            }else{
-                System.out.println("Jenkins NOT found the excel");
-            }
-
         }
 
 
