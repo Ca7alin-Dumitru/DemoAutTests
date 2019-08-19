d1a89253d68f472672cf2908155c6d0874c93387
Ca7alin-Dumitru <georgecatalin.dumitru@gmail.com>
2019-08-19 13:10:08 +0300

    testdata from excel

--- newtours_testdata/src/main/java/DataProviderForLoginTests.java
+++ newtours_testdata/src/main/java/DataProviderForLoginTests.java
@@ -19,7 +19,7 @@ public class DataProviderForLoginTests {
         ClassLoader classLoader = ClassLoader.getSystemClassLoader();
         File input = new File(classLoader.getResource("Login.xlsx").getFile());
 
-        if(true) {
+        if(input.exists() && !input.isDirectory()) {
             String myArray[][] = new String[2][2];
             int i = 0;
             try {
@@ -60,6 +60,50 @@ public class DataProviderForLoginTests {
             }
 
             return myArray;
+        }else{
+            input = new File(classLoader.getResource("C:\\Program Files (x86)\\Jenkins\\workspace\\AllureWithMaven\\newtours_testdata\\target\\classes\\Login.xlsx").getFile());
+            if(input.exists() && !input.isDirectory()) {
+                String myArray[][] = new String[2][2];
+                int i = 0;
+                try {
+                    FileInputStream file = new FileInputStream(input);
+
+                    // Create Workbook instance holding reference to .xlsx file
+                    HSSFWorkbook workbook = new HSSFWorkbook(file);
+
+                    // Get first/desired sheet from the workbook
+                    HSSFSheet sheet = workbook.getSheetAt(0);
+
+                    // Iterate through each rows one by one
+                    Iterator<Row> rowIterator = sheet.iterator();
+                    while (rowIterator.hasNext()) {
+                        Row row = rowIterator.next();
+                        // For each row, iterate through all the columns
+                        Iterator<Cell> cellIterator = row.cellIterator();
+                        int j = 0;
+                        while (cellIterator.hasNext()) {
+                            Cell cell = cellIterator.next();
+                            // Check the cell type and format accordingly
+                            switch (cell.getCellType()) {
+                                case NUMERIC:
+                                    myArray[i][j] = Double.toString(cell.getNumericCellValue());
+                                    break;
+                                case STRING:
+                                    myArray[i][j] = cell.getStringCellValue();
+                                    break;
+                            }
+                            j++;
+                        }
+                        i++;
+                    }
+                    file.close();
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                return myArray;
+            }
         }
 
 
