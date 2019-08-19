b3ae992697e0155644ef0e23ed80ffea577b42b9
Ca7alin-Dumitru <georgecatalin.dumitru@gmail.com>
2019-08-19 10:17:48 +0300

    testdata from excel

--- newtours_testdata/src/main/java/DataProviderForLoginTests.java
+++ newtours_testdata/src/main/java/DataProviderForLoginTests.java
@@ -63,7 +63,46 @@ public class DataProviderForLoginTests {
         }else{
             input = new File(classLoader.getResource("newtours_testdata/src/main/resources/Login.xlsx").getFile());
             if(input.exists() && !input.isDirectory()) {
-                System.out.println("Jenkins found the excel");
+                String myArray[][] = new String[2][2];
+                int i = 0;
+                try {
+                    FileInputStream file = new FileInputStream(input);
+
+                    // Create Workbook instance holding reference to .xlsx file
+                    HSSFWorkbook workbook = new HSSFWorkbook(file);
+
+                    // Get first/desired sheet from the workbook
+                    HSSFSheet sheet = workbook.getSheetAt(0);
+
+                    // Iterate through each rows one by one
+                    Iterator<Row> rowIterator = sheet.iterator();
+                    while (rowIterator.hasNext()) {
+                        Row row = rowIterator.next();
+                        // For each row, iterate through all the columns
+                        Iterator<Cell> cellIterator = row.cellIterator();
+                        int j = 0;
+                        while (cellIterator.hasNext()) {
+                            Cell cell = cellIterator.next();
+                            // Check the cell type and format accordingly
+                            switch (cell.getCellType()) {
+                                case NUMERIC:
+                                    myArray[i][j] = Double.toString(cell.getNumericCellValue());
+                                    break;
+                                case STRING:
+                                    myArray[i][j] = cell.getStringCellValue();
+                                    break;
+                            }
+                            j++;
+                        }
+                        i++;
+                    }
+                    file.close();
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                return myArray;
             }else{
                 System.out.println("Jenkins NOT found the excel");
             }
